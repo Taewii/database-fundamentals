cf34a8974489fad8367b550b0dfa76fff69a0c68
Taewii <p3rfec7@abv.bg>
2018-09-14 20:15:44 +0300

    problem solutions

--- Database Basics/02. Data Definition And Data Types - Exercises/p06_create_table_people.sql
+++ Database Basics/02. Data Definition And Data Types - Exercises/p06_create_table_people.sql
@@ -1,20 +1,18 @@
 CREATE TABLE people(
-	id INT AUTO_INCREMENT NOT NULL,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	`name` VARCHAR(200) NOT NULL,
 	picture BLOB,
 	height DOUBLE,
 	weight DOUBLE,
 	gender CHAR(1) NOT NULL,
 	birthdate DATE NOT NULL,
-	biography TEXT, 
-	CONSTRAINT pk_people PRIMARY KEY (id)
+	biography TEXT 
 );
 
 
-INSERT INTO people(`name`, gender, birthdate) users
-VALUES
+INSERT INTO people(`name`, gender, birthdate) VALUES
 ('test', 'm', '1914-01-01'),
 ('test1', 'm', '1914-01-01'),
 ('test2', 'm', '1914-01-01'),
 ('test3', 'm', '1914-01-01'),
-('test4', 'm', '1914-01-01')
+('test4', 'm', '1914-01-01');
